f1591c9c56b9cf7b86d2e8a1c08cbb6fafb84599
Vijay Khemka <vijaykhemka@fb.com>
2020-08-11 16:01:23 -0700

    Add support for dbus parameter
    Added support for parameter type dbus sensor and utility functions
    to read dbus sensor value.
    
    Signed-off-by: Vijay Khemka <vijaykhemka@fb.com>
    Change-Id: I04db0016f312edea095442a693500c3b4e571d6c

---
+++ dbusSensor.hpp
@@ -0,0 +1,39 @@
+#include "dbusUtils.hpp"
+
+#include <sdbusplus/bus.hpp>
+
+const char* sensorIntf = "xyz.openbmc_project.Sensor.Value";
+
+class DbusSensor
+{
+  public:
+    DbusSensor() = delete;
+    virtual ~DbusSensor() = default;
+
+    /** @brief Constructs DbusSensor
+     *
+     * @param[in] bus     - Handle to system dbus
+     * @param[in] path    - The Dbus path of sensor
+     */
+    DbusSensor(sdbusplus::bus::bus& bus, std::string path) :
+        bus(bus), path(path)
+    {
+        servName = getService(bus, path.c_str(), sensorIntf);
+    }
+
+    /** @brief Get sensor value property from D-bus interface */
+    double getSensorValue()
+    {
+        auto propValue = getDbusProperty(bus, servName.c_str(), path.c_str(),
+                                         sensorIntf, "Value");
+        return std::get<double>(propValue);
+    }
+
+  private:
+    /** @brief sdbusplus bus client connection. */
+    sdbusplus::bus::bus& bus;
+    /** @brief complete path for sensor */
+    std::string path;
+    /** @brief service name for the sensor daemon */
+    std::string servName;
+};
