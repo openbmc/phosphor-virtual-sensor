37ea561ac5b5209ee7ba9a547f68efdec09f9a89
Vijay Khemka <vijaykhemka@fb.com>
2020-08-21 18:55:07 -0700

    Disable some exprttk options
    Disabling some of expression parser exprtk parsing options to reduce
    image size.
    
    Signed-off-by: Vijay Khemka <vijaykhemka@fb.com>
    Change-Id: Ie4beea72231fea343f6e216ed7476d3eafb0bb97

---
+++ exprtkTools.hpp
@@ -0,0 +1,40 @@
+/*
+ * This define will disable the ability for expressions to have comments.
+ * Expressions that have comments when parsed with a build that has this
+ * option, will result in a compilation failure.
+ */
+#define exprtk_disable_comments
+/*
+ * This define will disable the loop-wise 'break' and 'continue'
+ * capabilities. Any expression that contains those keywords will result
+ * in a compilation failure.
+ */
+#define exprtk_disable_break_continue
+/*
+ * This define will disable the short-circuit '&' (and) and '|' (or)
+ * operators
+ */
+#define exprtk_disable_sc_andor
+/*
+ * This define will disable all enhanced features such as strength
+ * reduction and special function optimisations and expression specific
+ * type instantiations. This feature will reduce compilation times and
+ * binary sizes but will also result in massive performance degradation
+ * of expression evaluations.
+ */
+#define exprtk_disable_enhanced_features
+/*
+ * This define will disable all string processing capabilities. Any
+ * expression that contains a string or string related syntax will result
+ * in a compilation failure.
+ */
+ #define exprtk_disable_string_capabilities
+
+ #define exprtk_disable_rtl_io_file
+ #define exprtk_disable_rtl_vecops
+ #define exprtk_disable_return_statement
+ #define exprtk_disable_rtl_io
+ #define exprtk_disable_superscalar_unroll
+
+/* include main exprtk header library */
+#include <exprtk.hpp>
