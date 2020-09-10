67373a67b7e1227716598e09482ab65b5c2f58be
Vijay Khemka <vijaykhemka@fb.com>
2020-09-09 22:24:18 -0700

    Add signal based update
    update sensors whenever there is a update in parameter
    dbus sensor. This will register a signal for each dbus
    sensor parameter and will calculate new value is there
    is a change in any param sensor value.
    
    Signed-off-by: Vijay Khemka <vijaykhemka@fb.com>
    Change-Id: I164bbb32eea6f29a2378119cdf3ac07492758b09

--- dbusSensor.hpp
+++ dbusSensor.hpp
@@ -1,8 +1,11 @@
 #include "dbusUtils.hpp"
 
 #include <sdbusplus/bus.hpp>
+#include <sdbusplus/server.hpp>
 
 const char* sensorIntf = "xyz.openbmc_project.Sensor.Value";
+
+int handleDbusSignal(sd_bus_message* msg, void* usrData, sd_bus_error* err);
 
 class DbusSensor
 {
@@ -15,10 +18,21 @@ class DbusSensor
      * @param[in] bus     - Handle to system dbus
      * @param[in] path    - The Dbus path of sensor
      */
-    DbusSensor(sdbusplus::bus::bus& bus, const std::string& path) :
-        bus(bus), path(path)
+    DbusSensor(sdbusplus::bus::bus& bus, const std::string& path, void* ctx) :
+        bus(bus), path(path),
+        signal(bus, getSensorMatch().c_str(), handleDbusSignal, ctx)
     {
         servName = getService(bus, path, sensorIntf);
+    }
+
+    /** @brief Get sensor match string for sensor signal */
+    std::string getSensorMatch()
+    {
+        return std::string("type='signal',"
+                           "interface='org.freedesktop.DBus.Properties',"
+                           "member='PropertiesChanged',"
+                           "path='" +
+                           path + "'");
     }
 
     /** @brief Get sensor value property from D-bus interface */
@@ -35,4 +49,6 @@ class DbusSensor
     std::string path;
     /** @brief service name for the sensor daemon */
     std::string servName;
+    /** @brief signal for sensor value change */
+    sdbusplus::server::match::match signal;
 };
