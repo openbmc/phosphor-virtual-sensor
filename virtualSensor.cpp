761163dc0255afee455534b387d634cc9e470fe8
Vijay Khemka <vijaykhemka@fb.com>
2020-09-10 14:45:49 -0700

    Fix threshold data copy
    Removed storing threshold data in the class as it is being stored
    in sensor threshold object.
    
    Signed-off-by: Vijay Khemka <vijaykhemka@fb.com>
    Change-Id: I86a96c576531c3c2fd62110792791ab2083012ab

--- virtualSensor.cpp
+++ virtualSensor.cpp
@@ -78,6 +78,7 @@ void VirtualSensor::initVirtualSensor(const Json& sensorConfig)
     auto threshold = sensorConfig.value("Threshold", empty);
     if (!threshold.empty())
     {
+        Threshold sensorThreshold;
         sensorThreshold.criticalHigh =
             threshold.value("CriticalHigh", defaultHighThreshold);
         sensorThreshold.criticalLow =
@@ -86,10 +87,10 @@ void VirtualSensor::initVirtualSensor(const Json& sensorConfig)
             threshold.value("WarningHigh", defaultHighThreshold);
         sensorThreshold.warningLow =
             threshold.value("WarningLow", defaultLowThreshold);
-    }
 
-    /* Set threshold value to dbus */
-    setSensorThreshold();
+        /* Set threshold value to dbus */
+        setSensorThreshold(sensorThreshold);
+    }
 
     /* Get expression string */
     exprStr = sensorConfig.value("Expression", "");
@@ -164,7 +165,7 @@ void VirtualSensor::setSensorValue(double value)
     ValueIface::value(value);
 }
 
-void VirtualSensor::setSensorThreshold()
+void VirtualSensor::setSensorThreshold(Threshold& sensorThreshold)
 {
     CriticalInterface::criticalHigh(sensorThreshold.criticalHigh);
     CriticalInterface::criticalLow(sensorThreshold.criticalLow);
