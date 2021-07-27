5d8723e63f5b679a6fc67c93cdb16b79c6079b05
Rashmica Gupta <rashmica.g@gmail.com>
2021-07-27 19:42:34 +1000

    Add a modified median calculation
    This is for virtual sensors added via D-Bus.
    
    We use the median value if there is 3 or more valid values. If there
    are only two valid values we used the biggest and if there is only one
    valid value we use that.
    
    Tested:
    - Modified temperature values of sensors like this:
    
    busctl set-property xyz.openbmc_project.HwmonTempSensor
    /xyz/openbmc_project/sensors/temperature/Ambient_0_Temp
    xyz.openbmc_project.Sensor.Value Value d 30
    
    busctl set-property xyz.openbmc_project.IIOSensor
    /xyz/openbmc_project/sensors/temperature/Ambient_1_Temp
    xyz.openbmc_project.Sensor.Value Value d 60
    
    and expected changes were observed in the value of the Ambient Virtual
    Temp value.
    
    busctl get-property xyz.openbmc_project.VirtualSensor
    /xyz/openbmc_project/sensors/temperature/Ambient_Virtual_Temp
    xyz.openbmc_project.Sensor.Value Value
    
    - Threshold alarms were asserted and deasserted when expected.
    
    - Temperature values were not used when out of range.
    
    Change-Id: I58ff7dcc17c6f87209434a754f91a99f483140aa
    Signed-off-by: Rashmica Gupta <rashmica.g@gmail.com>

--- virtualSensor.cpp
+++ virtualSensor.cpp
@@ -18,7 +18,8 @@ static constexpr auto entityManagerBusName =
 static constexpr auto vsConfigIntfPrefix = "xyz.openbmc_project.Configuration.";
 static constexpr auto vsDBusParamsIntfSuffix = ".Sensors";
 static constexpr auto vsThresholdsIntfSuffix = ".Thresholds";
-static constexpr std::array<const char*, 0> calculationTypes = {};
+static constexpr std::array<const char*, 1> calculationTypes = {
+    "modifiedMedian"};
 
 using namespace phosphor::logging;
 
@@ -370,7 +371,8 @@ void VirtualSensor::setSensorValue(double value)
     ValueIface::value(value);
 }
 
-double VirtualSensor::calculateValue(const std::string& calculation)
+double VirtualSensor::calculateValue(const std::string& calculation,
+                                     const VirtualSensor::ParamMap& paramMap)
 {
     auto itr = std::find(calculationTypes.begin(), calculationTypes.end(),
                          calculation);
@@ -378,7 +380,20 @@ double VirtualSensor::calculateValue(const std::string& calculation)
     {
         return std::numeric_limits<double>::quiet_NaN();
     }
+    else if (calculation == "modifiedMedian")
+    {
+        return calculateModifiedMedianValue(paramMap);
+    }
     return std::numeric_limits<double>::quiet_NaN();
+}
+
+bool VirtualSensor::sensorInRange(double value)
+{
+    if (value <= this->maxValidInput && value >= this->minValidInput)
+    {
+        return true;
+    }
+    return false;
 }
 
 void VirtualSensor::updateVirtualSensor()
@@ -406,7 +421,7 @@ void VirtualSensor::updateVirtualSensor()
     }
     else
     {
-        val = calculateValue(exprStr);
+        val = calculateValue(exprStr, paramMap);
     }
     /* Set sensor value to dbus interface */
     setSensorValue(val);
@@ -422,6 +437,52 @@ void VirtualSensor::updateVirtualSensor()
     checkThresholds(val, criticalIface);
     checkThresholds(val, softShutdownIface);
     checkThresholds(val, hardShutdownIface);
+}
+
+double VirtualSensor::calculateModifiedMedianValue(
+    const VirtualSensor::ParamMap& paramMap)
+{
+    std::vector<double> values;
+
+    for (auto& param : paramMap)
+    {
+        auto& name = param.first;
+        if (auto var = symbols.get_variable(name))
+        {
+            if (!sensorInRange(var->ref()))
+            {
+                continue;
+            }
+            values.push_back(var->ref());
+        }
+    }
+
+    double val;
+    size_t size = values.size();
+    std::sort(values.begin(), values.end());
+    switch (size)
+    {
+        case 2:
+            /* Choose biggest value */
+            val = values.at(1);
+            break;
+        case 0:
+            val = std::numeric_limits<double>::quiet_NaN();
+            break;
+        default:
+            /* Choose median value */
+            if (size % 2 == 0)
+            {
+                // Average of the two middle values
+                val = (values.at(size / 2) + values.at(size / 2 - 1)) / 2;
+            }
+            else
+            {
+                val = values.at((size - 1) / 2);
+            }
+            break;
+    }
+    return val;
 }
 
 void VirtualSensor::createThresholds(const Json& threshold,
