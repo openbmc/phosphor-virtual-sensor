925cbc58e9161e5a9217d17c4c48c31a2819109e
Tao Lin <lintao.lc@ieisystem.com>
2023-10-31 17:38:17 +0800

    UpdateVirtualSensor uses information from signals
    UpdateVirtualSensor uses information obtained from signals to avoid
    extensive dbus queries.
    
    Tested:
    1:Execute DBus cmd to see if VirtualSensor can correctly obtain the
    value of DbusSensor
        busctl tree xyz.openbmc_project.VirtualSensor
        busctl introspect xyz.openbmc_project.VirtualSensor xxx
    2:Waiting for the value change of DbusSensor,Check if the value of
    the virtual sensor has changed after the dbusSensor changes.
    
    Link: https://github.com/openbmc/phosphor-virtual-sensor/issues/1
    Change-Id: If11f9017b31ce5cf06f910a38c65637c55d74b24
    Signed-off-by: Tao Lin <lintao.lc@ieisystem.com>

--- dbusSensor.hpp
+++ dbusSensor.hpp
@@ -6,6 +6,19 @@
 const char* sensorIntf = "xyz.openbmc_project.Sensor.Value";
 
 int handleDbusSignal(sd_bus_message* msg, void* usrData, sd_bus_error* err);
+int handleDbusSignalRemove(sd_bus_message* msg, void* usrData,
+                           sd_bus_error* err);
+int handleDbusSignalAdd(sd_bus_message* msg, void* usrData, sd_bus_error* err);
+
+using AssociationList =
+    std::vector<std::tuple<std::string, std::string, std::string>>;
+using DBusValue = std::variant<std::string, bool, double, std::vector<uint8_t>,
+                               std::vector<std::string>, AssociationList,
+                               std::tuple<uint64_t, std::vector<uint8_t>>>;
+using DBusProperty = std::string;
+using DBusInterface = std::string;
+using DBusPropertyMap = std::map<DBusProperty, DBusValue>;
+using DBusInterfaceMap = std::map<DBusInterface, DBusPropertyMap>;
 
 class DbusSensor
 {
@@ -24,29 +37,79 @@ class DbusSensor
             bus,
             sdbusplus::bus::match::rules::propertiesChanged(path, sensorIntf),
             handleDbusSignal, ctx)
-    {}
+    {
+        auto matchAdd = std::make_unique<sdbusplus::bus::match_t>(
+            bus,
+            sdbusplus::bus::match::rules::interfacesAdded() +
+                sdbusplus::bus::match::rules::argNpath(0, path),
+            handleDbusSignalAdd, ctx);
+        matches.emplace_back(std::move(matchAdd));
 
-    /** @brief Get sensor value property from D-bus interface */
+        auto matchRemove = std::make_unique<sdbusplus::bus::match_t>(
+            bus,
+            sdbusplus::bus::match::rules::interfacesRemoved() +
+                sdbusplus::bus::match::rules::argNpath(0, path),
+            handleDbusSignalRemove, ctx);
+        matches.emplace_back(std::move(matchRemove));
+
+        clearSensorValue();
+
+        updateSensorValue();
+    }
+
+    /** @brief Set sensor value */
+    void setSensorValue(double sensorValue)
+    {
+        value = sensorValue;
+    }
+
+    /** @brief Get sensor value from local */
     double getSensorValue()
     {
-        if (servName.empty())
-        {
-            servName = getService(bus, path, sensorIntf);
-            if (servName.empty())
-            {
-                return std::numeric_limits<double>::quiet_NaN();
-            }
-        }
+        return value;
+    }
 
+    /** @brief Get sensor Path from local */
+    std::string getSensorPath()
+    {
+        return path;
+    }
+
+    /** @brief Get sensor dbus service name from local */
+    std::string getSensorServName()
+    {
+        return servName;
+    }
+
+    /** @brief clear sensor value */
+    void clearSensorValue()
+    {
+        value = std::numeric_limits<double>::quiet_NaN();
+    }
+
+    /** @brief Get sensor value property from D-bus interface */
+    void updateSensorValue()
+    {
         try
         {
-            return getDbusProperty<double>(bus, servName, path, sensorIntf,
-                                           "Value");
+            servName = getService(bus, path, sensorIntf);
+
+            if (servName.empty())
+            {
+                value = std::numeric_limits<double>::quiet_NaN();
+            }
+            else
+            {
+                value = getDbusProperty<double>(bus, servName, path, sensorIntf,
+                                                "Value");
+            }
         }
         catch (const std::exception& e)
         {
-            return std::numeric_limits<double>::quiet_NaN();
+            value = std::numeric_limits<double>::quiet_NaN();
         }
+
+        return;
     }
 
   private:
@@ -58,4 +121,10 @@ class DbusSensor
     std::string servName{};
     /** @brief signal for sensor value change */
     sdbusplus::bus::match_t signal;
+
+    /** @brief Matches for this dbus sensor */
+    std::vector<std::unique_ptr<sdbusplus::bus::match_t>> matches{};
+
+    /** @brief dbus sensor value */
+    double value;
 };
