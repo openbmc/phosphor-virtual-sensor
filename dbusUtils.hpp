134c4e2b4d3410f141a82df1494e4c4a3d79a6ec
George Liu <liuxiwei@ieisystem.com>
2024-10-23 14:08:46 +0800

    Refactor dbus utils
    Optimize dbus utils and remove templates of getDbusProperty.
    
    Change-Id: I224277308e1d78eaef2d60843eccdfa6d9e69436
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- dbusUtils.hpp
+++ dbusUtils.hpp
@@ -1,41 +1,17 @@
 #pragma once
 
-#include <phosphor-logging/elog-errors.hpp>
-#include <phosphor-logging/lg2.hpp>
-#include <xyz/openbmc_project/Common/error.hpp>
+#include <sdbusplus/server.hpp>
 
-using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+#include <string>
 
 using Value = std::variant<int64_t, double, std::string, bool>;
 
 std::string getService(sdbusplus::bus_t& bus, const std::string& path,
                        const char* intf);
 
-template <typename T>
-
-T getDbusProperty(sdbusplus::bus_t& bus, const std::string& service,
-                  const std::string& path, const std::string& intf,
-                  const std::string& property)
-{
-    Value value;
-
-    auto method = bus.new_method_call(service.c_str(), path.c_str(),
-                                      "org.freedesktop.DBus.Properties", "Get");
-
-    method.append(intf, property);
-
-    try
-    {
-        auto msg = bus.call(method);
-        msg.read(value);
-    }
-    catch (const sdbusplus::exception_t& ex)
-    {
-        return std::numeric_limits<T>::quiet_NaN();
-    }
-
-    return std::get<T>(value);
-}
+Value getDbusProperty(sdbusplus::bus_t& bus, const std::string& service,
+                      const std::string& path, const std::string& intf,
+                      const std::string& property);
 
 int setDbusProperty(sdbusplus::bus_t& bus, const std::string& service,
                     const std::string& path, const std::string& intf,
