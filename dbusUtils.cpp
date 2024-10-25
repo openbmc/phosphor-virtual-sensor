ba52c029b87e307f0b6e2d1de2b2e0ece0ca52b8
George Liu <liuxiwei@ieisystem.com>
2024-10-25 14:42:26 +0800

    dbusUtils: Optimize the getService method
    When the getService method fails to be called, the error log is
    directly recorded and an empty string is returned without throwing an
    exception again.
    
    Change-Id: Icc801abf3ce588b1d0391c03ab47d3beb3d8ac3a
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- dbusUtils.cpp
+++ dbusUtils.cpp
@@ -1,31 +1,25 @@
 #include "dbusUtils.hpp"
 
 #include <phosphor-logging/lg2.hpp>
-#include <xyz/openbmc_project/Common/error.hpp>
-#include <xyz/openbmc_project/ObjectMapper/common.hpp>
 
 #include <unordered_map>
 
 constexpr auto propIntf = "org.freedesktop.DBus.Properties";
 constexpr auto mapperBusName = "xyz.openbmc_project.ObjectMapper";
 constexpr auto mapperPath = "/xyz/openbmc_project/object_mapper";
-constexpr auto mapperIntf =
-    sdbusplus::common::xyz::openbmc_project::ObjectMapper::interface;
+constexpr auto mapperIntf = "xyz.openbmc_project.ObjectMapper";
+
 constexpr auto methodGetObject = "GetObject";
 constexpr auto methodGet = "Get";
 constexpr auto methodSet = "Set";
 
-using namespace sdbusplus::xyz::openbmc_project::Common::Error;
-
 std::string getService(sdbusplus::bus_t& bus, const std::string& path,
-                       const char* intf)
+                       const std::string& intf)
 {
     /* Get mapper object for sensor path */
     auto mapper = bus.new_method_call(mapperBusName, mapperPath, mapperIntf,
                                       methodGetObject);
-
-    mapper.append(path.c_str());
-    mapper.append(std::vector<std::string>({intf}));
+    mapper.append(path, std::vector<std::string>({intf}));
 
     std::unordered_map<std::string, std::vector<std::string>> resp;
 
@@ -33,30 +27,19 @@ std::string getService(sdbusplus::bus_t& bus, const std::string& path,
     {
         auto msg = bus.call(mapper);
         msg.read(resp);
+
+        if (!resp.empty())
+        {
+            return resp.begin()->first;
+        }
     }
     catch (const sdbusplus::exception_t& ex)
     {
-        if (ex.name() == std::string(ResourceNotFound::errName))
-        {
-            // The service isn't on D-Bus yet.
-            return std::string{};
-        }
-        else if (ex.name() == std::string("org.freedesktop.DBus.Error.Timeout"))
-        {
-            lg2::info("Mapper timeout while looking up {PATH}", "PATH", path);
-            return std::string{};
-        }
-
-        throw;
+        lg2::error("Failed to get service for path {PATH}: {ERROR}", "PATH",
+                   path, "ERROR", ex);
     }
 
-    if (resp.begin() == resp.end())
-    {
-        // Shouldn't happen, if the mapper can't find it it is handled above.
-        throw std::runtime_error("Unable to find Object: " + path);
-    }
-
-    return resp.begin()->first;
+    return std::string{};
 }
 
 Value getDbusProperty(sdbusplus::bus_t& bus, const std::string& service,
